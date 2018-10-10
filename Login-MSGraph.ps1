c5e3ff275c70d1fbb2302cd1b466ba60aed07699
clebarge <43185921+clebarge@users.noreply.github.com>
2018-10-10 16:26:46 -0700

    update

--- Login-MSGraph.ps1
+++ Login-MSGraph.ps1
@@ -1,6 +1,9 @@
 ﻿<#
-Login to Microsoft Graph
+Login to Microsoft Graph.
 This script is intended for testing to determine the minimum code required to connect to Microsoft Graph through PowerShell.
+
+Information provided by Microsoft at https://blogs.technet.microsoft.com/cloudlojik/2018/06/29/connecting-to-microsoft-graph-with-a-native-app-using-powershell/ forms
+the basis of this work.
 
 About Graph
 Graph is actually an Application Programing Interface (API), not a service in itself, as such you do not actually connect to Graph from within Powershell.
@@ -12,7 +15,40 @@ Microsoft has made Graph available through the Azure AD App Registration system,
     Using Web API to connect to AzureAD 2.0 Web App connecitng to Graph.
 
     As this is a new project, the focus is on the most current and first to be understood Azure AD 2.0 Native App with REST API.
+    Please follow the instructions in document "Login-MSGraph Create Azure AD 2.0 App Registration.docx."
 
-Because Graph is an API, in general if you need to pull information from Office 365, Azure, or other service using the web interface Graph Explorer or implementing a connection
+Because Graph is an API, in general if you need to pull information from Office 365 and Azure AD using the web interface Graph Explorer or implementing a connection
 through Power BI will be preferable. The most likely reason to use PowerShell is to have scripted automation in response to information in Graph, or perhaps in IF this then That situations.
+
+Login-MSGraph
+    [-AppName] <string>
+
+This script only logs on and connects to Graph. It performs no other actions.
+
 #>
+
+#Command Line Parameters
+param(
+[parameter(Mandatory=$false,HelpMessage="Specify an App Name, if omitted default is LoginGraphAAD2")][string]$AppName="LoginGraphAAD2"
+
+)
+
+#Required Variables
+    
+    #This variable is used in the creation of the access token for the content in Graph.
+    $resourceAppIdURI = "https://graph.microsoft.com"
+
+    #Credential Prompt Behaviour. This value modifies how, the connection to Graph will deal with credentials.
+        #Auto - This is the default behaviour, if credentials already exist, it uses the existing credential. It will prompt if more than one credential exists that are valid.
+        #Always - You'll always be prompted to pick a login or provide a login.
+        #Never - You won't be prompted, however if more than one credential exists it will fail silently.
+        #RefreshSession - For when/if your session has expired, usually after 1 hour of inactivity.
+    $CredPrompt       = "Auto"                                   #Auto, Always, Never, RefreshSession
+
+###Begin Body###
+
+#We need to connect, or to have connected already to the Azure AD 2.0 endpoint.
+#This code is a bit of advanced powershell scripting I don't fully understand. Taken from example provided by Microsoft. 
+
+#The connection requires the Application's ID
+    $AppId = (Get-AzureADApplication -SearchString $AppName).AppId
